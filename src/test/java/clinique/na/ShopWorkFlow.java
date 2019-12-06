9d9b7184f098f59aea87de4adbdab9b30df4a04b
Daniel Ionita <daniel.ionita@softvision.ro>
2019-12-06 14:39:22 +0200

    implemented WebDriverWait methods in ShopWF test

--- src/test/java/clinique/na/ShopWorkFlow.java
+++ src/test/java/clinique/na/ShopWorkFlow.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -45,10 +47,10 @@ public class ShopWorkFlow {
         String baseUrl = "https://e.clinique.na.us.stage.ncsa.elcdev.net/account/index.tmpl";
         driver.manage().window().maximize();
         driver.navigate().to(baseUrl);
-        action.sendKeys("clinique").build().perform();
+/*        action.sendKeys("clinique").build().perform();
         action.sendKeys(Keys.TAB).build().perform();
         action.sendKeys("5upuq%ic").build().perform();
-        action.sendKeys(Keys.ENTER).build().perform();
+        action.sendKeys(Keys.ENTER).build().perform();*/
 
 
         //https://e.clinique.na.us.stage.ncsa.elcdev.net/account/index.tmpl
@@ -95,27 +97,35 @@ public class ShopWorkFlow {
 
     @Test(priority = 2)
     public void buyProduct() throws InterruptedException {
-        Thread.sleep(3500);
+        WebDriverWait wait = new WebDriverWait(driver,30);
+        //Thread.sleep(3500);
         action.sendKeys(Keys.ESCAPE).build().perform();
         Assert.assertTrue(homepage.getFoundationTabBtn().isDisplayed());
         action.moveToElement(homepage.getFoundationTabBtn()).perform();
-        Thread.sleep(3500);
+        //Thread.sleep(3500);
+        wait.until(ExpectedConditions.elementToBeClickable(homepage.getFoundationTabBtn()));
         action.click(homepage.getFoundationTabBtn()).build().perform();
-        Thread.sleep(1500);
+        //Thread.sleep(1500);
+        wait.until(ExpectedConditions.elementToBeClickable(foundation.getFoundationsBanner()));
         Assert.assertTrue(foundation.getFoundationsBanner().isDisplayed());
-        Thread.sleep(1500);
+        //Thread.sleep(1500);
+        wait.until(ExpectedConditions.visibilityOf(foundation.getNewProduct()));
         Assert.assertTrue(foundation.getNewProduct().isDisplayed());
-        Thread.sleep(1500);
+        //Thread.sleep(1500);
         //action.moveToElement(foundation.getNewProduct()).perform();
+        wait.until(ExpectedConditions.elementToBeClickable(foundation.getNewProduct()));
         action.click(foundation.getNewProduct()).build().perform();
-        Thread.sleep(3500);
+        //Thread.sleep(3500);
+        wait.until(ExpectedConditions.elementToBeClickable(foundation.getProductShade()));
         action.click(foundation.getProductShade()).build().perform();
-        Thread.sleep(2500);
+        //Thread.sleep(2500);
+        wait.until(ExpectedConditions.elementToBeClickable(foundation.getShadeLevel5()));
         action.click(foundation.getShadeLevel5()).build().perform();
-        Thread.sleep(2500);
+        //Thread.sleep(2500);
 /*        action.click(foundation.getAddToBagShadeBtn()).build().perform();
         Thread.sleep(3500);*/
         if(driver.findElement(By.xpath("//img[@alt='Close chat']")).isDisplayed()) {
+            wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//img[@alt='Close chat']")));
             driver.findElement(By.xpath("//img[@alt='Close chat']")).click();
         }
         //driver.findElement(By.xpath("//div[@id='content']/div/div/div/div/div/div/div/div/div")).click();
@@ -124,31 +134,33 @@ public class ShopWorkFlow {
             driver.findElement(By.xpath("//img[@alt='Close chat']")).click();
         }*/
         //action.click(cart.getChatPopup()).build().perform();
-        Thread.sleep(3500);
+        //Thread.sleep(3500);
+        wait.until(ExpectedConditions.elementToBeClickable(foundation.getAddToBagBtn()));
         action.click(foundation.getAddToBagBtn()).perform();
-        Thread.sleep(2500);
+        //Thread.sleep(2500);
         //action.click(cart.getCheckoutBtn()).perform();
         //Thread.sleep(1500);
         //action.click(cart.getCheckoutBtn()).build().perform();
         //driver.findElement(By.className("button btn-primary")).click();
         //driver.findElement(By.linkText("Checkout")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".btn-primary")));
         driver.findElement(By.cssSelector(".btn-primary")).click();
         //driver.findElement(By.partialLinkText("Checko")).click();
-        Thread.sleep(1500);
+        //Thread.sleep(1500);
         Assert.assertTrue(cart.getProductsInCart().isDisplayed());
-
+        wait.until(ExpectedConditions.elementToBeClickable(cart.getBagIconBtn()));
         action.click(cart.getBagIconBtn()).build().perform();
-        Thread.sleep(1500);
+        //Thread.sleep(1500);
         Assert.assertEquals(cart.getQuantityOfProductsInCart().getText(), "1");
-        Thread.sleep(1500);
+        //Thread.sleep(1500);
         Assert.assertEquals(cart.getProductNameInCart().getText(), cart.getProductsInCart().getText());
-        Thread.sleep(1500);
+        //Thread.sleep(1500);
         action.click(cart.getCloseCartModal()).build().perform();
-        Thread.sleep(1500);
+        //Thread.sleep(1500);
         action.click(cart.getRemoveProductsFromCart()).perform();
-        Thread.sleep(1500);
+        //Thread.sleep(1500);
         action.click(cart.getRemoveProductsFromCart()).build().perform();
-        Thread.sleep(1500);
+        //Thread.sleep(1500);
 /*        action.click(homepage.getFoundationTabBtn()).build().perform();
         Thread.sleep(1500);
         Assert.assertTrue(homepage.getFoundationTabBtn().isDisplayed());*/
